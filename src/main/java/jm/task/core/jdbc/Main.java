b9c7a37a2717d79772661cba329d16a25f83ce76
BulatRinchinov <rinchinovbb@mail.ru>
2021-06-16 01:47:57 +0800

    Revert "Задача 1.1.3 Ринчинов Булат 16-06-2021"
    This reverts commit 9244fcfd

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,21 +1,7 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.service.UserService;
-import jm.task.core.jdbc.service.UserServiceImpl;
-
-import java.sql.SQLException;
-
 public class Main {
-    public static void main(String[] args) throws SQLException {
-        UserService userService = new UserServiceImpl();
-        userService.createUsersTable();
-        userService.saveUser("Александр", "Ветров",(byte)50);
-        userService.saveUser("Владимир", "Игнатьев",(byte)24);
-        userService.saveUser("Жаргал", "Дашиев",(byte)25);
-        userService.saveUser("Ирина", "Насанова",(byte)39);
-        userService.getAllUsers();
-        userService.cleanUsersTable();
-        userService.dropUsersTable();
-
+    public static void main(String[] args) {
+        // реализуйте алгоритм здесь
     }
 }
